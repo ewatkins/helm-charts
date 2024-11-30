48b08624ed3884abbb4ad24f14afd04c53fe83c1
Eric Watkins <eric@ewatkins.dev>
2024-11-29 22:33:23 -0600

    fix: Add Token for Release

--- .github/ci/kubeconform.sh
+++ .github/ci/kubeconform.sh
@@ -1,44 +1,23 @@
 #!/bin/bash
-
-mkdir -p ./.bin
-export PATH="./.bin:$PATH"
-
 set -euxo pipefail
 
 # renovate: datasource=github-releases depName=kubeconform packageName=yannh/kubeconform
 KUBECONFORM_VERSION=v0.6.7
 
-# renovate: datasource=github-releases depName=semver2 packageName=Ariel-Rodriguez/sh-semversion-2
-SEMVER_VERSION=v1.0.5
-
 # CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | cut -d '/' -f 2 | uniq)"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
 
 # install kubeconform
-curl --silent --show-error --fail --location --output /tmp/kubeconform.tar.gz https://github.com/yannh/kubeconform/releases/download/"${KUBECONFORM_VERSION}"/kubeconform-linux-arm64.tar.gz
-tar -C .bin/ -xf /tmp/kubeconform.tar.gz kubeconform
-
-# install semver compare
-curl -sSfLo .bin/semver2 https://raw.githubusercontent.com/Ariel-Rodriguez/sh-semversion-2/${SEMVER_VERSION}/semver2.sh
-chmod +x .bin/semver2
-
-# Compute required kubernetes api versions
-apis=()
-
-if [[ "$(semver2 "${KUBERNETES_VERSION#v}" 1.21.0)" -ge 0 ]]; then
-  apis=("${apis[@]}" --api-versions batch/v1/CronJob)
-else
-  apis=("${apis[@]}" --api-versions batch/v1beta1/CronJob)
-fi
+brew install kubeconform
 
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
-  (cd "charts/${CHART_DIR}"; helm dependency build)
-  for VALUES_FILE in charts/"${CHART_DIR}"/ci/*.yaml; do
+  (cd "${CHART_DIR}"; helm dependency build)
+  for VALUES_FILE in "${CHART_DIR}"/ci/*.yaml; do
     helm template \
-      "${apis[@]}" \
+      --api-versions batch/v1/CronJob \
       --values "${VALUES_FILE}" \
-      charts/"${CHART_DIR}" | kubeconform \
+      "${CHART_DIR}" | kubeconform \
         --strict \
         --ignore-missing-schemas \
         --kubernetes-version "${KUBERNETES_VERSION#v}" \
